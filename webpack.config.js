aa015d1d3b45aaf7210616edce2fc9e56eba4811
Lyudmila Zahrevska <lyudmila.zahrevska@nixsolutions.com>
2019-08-22 15:37:47 +0300

    code clining

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// var webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
