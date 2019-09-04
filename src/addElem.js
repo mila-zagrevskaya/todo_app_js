91af80ddf7661a02f28321ad58a4c0d0b6e9a5fb
Lyudmila Zahrevska <lyudmila.zahrevska@nixsolutions.com>
2019-09-04 16:22:11 +0300

    fixed bugs, make an active screen from an emptyScreen, changed createElements

--- src/addElem.js
+++ src/addElem.js
@@ -12,14 +12,3 @@ export const createElementWithAttributes = (
   eventType && elem.addEventListener(eventType, eventHandler);
   return elem;
 };
-
-
-export const addElem = ({
-  tagName, container, className, text, id,
-}) => {
-  const attributes = { };
-  className && (attributes.className = className);
-  text && (attributes.textContent = text);
-  id && (attributes.id = id);
-  return createElementWithAttributes({ tagName, container, attributes });
-};
