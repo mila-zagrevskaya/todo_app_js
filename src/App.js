7ee7d2cd8c8fc258d6fcd6af284d251553b6a43b
Lyudmila Zahrevska <lyudmila.zahrevska@nixsolutions.com>
2019-09-05 18:56:39 +0300

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Modal } from './Modal';
 import {
-  tasksUrl, activeTasksUrl, completedTasksUrl, expiredTasksUrl,
+  tasksUrl, tasksArchiveUrl, activeTasksUrl, completedTasksUrl, expiredTasksUrl,
 } from './constans';
 import { tasksContainer, containerForEmptyScreen } from './wrapper';
 import { controlBar, tasksList } from './index';
@@ -42,8 +42,8 @@ export class App {
 
   renderArchiveTasksScreen = async () => {
     this._cleanContainers();
-    const items = await this._getItems(expiredTasksUrl);
-    const doneItems = await this._getItems(completedTasksUrl);
+    const items = await this._getItems(tasksArchiveUrl);
+    // const doneItems = await this._getItems(completedTasksUrl);
     controlBar.renderControlBar('expired');
     tasksList.updateItems(items);
   }
--- src/constans.js
+++ src/constans.js
@@ -1,7 +1,7 @@
 export const basicUrl = 'http://localhost:3000/';
 export const tasksUrl = `${basicUrl}tasks`;
 
-// export const tasksArchiveUrl = `${tasksUrl}/?doneStatus=true&expired=true`;
+export const tasksArchiveUrl = `${tasksUrl}/?doneStatus=true&?expired=true`;
 export const activeTasksUrl = `${tasksUrl}/?doneStatus=false&expired=false`;
 
 export const expiredTasksUrl = `${tasksUrl}/?expired=true`;
